e45dd9db4be98a702c7d5124760a1c8f350c4229
Yuta Kawasaki <yuta.kawasaki@aist.go.jp>
2015-06-04 16:29:38 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# nw-songle-widget
+
+Songle Widget + NW.js Example
+
+## Usage
+
+```
+$ cd nw-songle-widget
+$ npm install && npm start
+```
---
+++ script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var notifier = require('node-notifier');
+var gui = require('nw.gui');
+var w = gui.Window.get();
+
+w.on('new-win-policy', function(frame, url, policy) {
+  gui.Shell.openExternal(url);
+  policy.ignore();
+});
+
+window.onSongleWidgetReady = function(apiKey, songleWidget) {
+  ['repeatSegmentEnter', 'chorusSemgnetEnter'].forEach(function(name) {
+    songleWidget.on(name, function(e) {
+      notifier.notify({
+        'title': 'Songle Widget',
+        'message': name
+      });
+    });
+  });
+};
